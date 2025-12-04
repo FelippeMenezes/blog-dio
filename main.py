13dcd83bcf1d006d015f4fc2c16f0843f9287ce9
Felippe Menezes <felippemenezes1982@gmail.com>
2025-12-04 15:44:37 -0300

    Modelo de dados

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-from fastapi import FastAPI
-from controllers import post
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+import databases
+import sqlalchemy as sa
+from controllers import post
+from fastapi import FastAPI
+
+DATABASE_URL = "sqlite:///./blog.db"
+
+database = databases.Database(DATABASE_URL)
+metadata = sa.MetaData()
+engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+@asynccontextmanager
+async def applifespan(app: FastAPI):
+    from models.post import posts #noqa
+    await database.connect()
+    metadata.create_all(engine)
+    yield
+    await database.disconnect()
+
+app = FastAPI(lifespan=applifespan)
 app.include_router(post.router)
+
+
+
+
+
+
+
+
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+
--- views/post.py
+++ views/post.py
@@ -3,5 +3,5 @@ from datetime import UTC, datetime
 
 class PostOut(BaseModel):
     title: str
-    date: datetime = datetime.now(UTC)
-    published: bool = False
+    content: str
+    published_at: datetime | None
